c8f9a605f71e4565de64bd66756be0939438af81
Igor Tchone Riba <132633397+IgorTchone@users.noreply.github.com>
2024-12-28 22:37:59 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+        for (String language : list) {
+            System.out.println(languageMain);
+        }
+    }
+}
